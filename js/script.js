fe33795a958f3f6708aacbb94f68c1cb34a82b57
jacopo35 <jaczor00@gmail.com>
2021-12-14 11:21:18 +0100

    add-js-let&function

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ Scrivere una funzione(e lanciarla : razzo: ) che accetti tre argomenti, un array
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.
 */
+
+let arrayNames = ['Jacopo', 'Giada', 'Riccardo', 'Luca', 'Barbara', 'Enrico'];
+
+let a = 2;
+let b = 5;
+
+console.log('Array originale: ' + arrayNames);
+console.log('a: ' + a + ' , b: ' + b);
+
+let arrayNew = getNames(arrayNames, a, b);
+console.log('Array nuovo: ' + arrayNew);
+
+function getNames(array, num1, num2) {
+    let arrayNew = array.filter((element, index) => {
+        return index >= num1 && index <= num2;
+    });
+    return arrayNew;
+}
